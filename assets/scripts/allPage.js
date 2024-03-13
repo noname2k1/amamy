7d33e37cedb94346a25983b496509027dac04edd
ninhnam <ninhnamyb2001@gmail.com>
2024-03-13 14:42:57 +0700

    add

--- assets/scripts/allPage.js
+++ assets/scripts/allPage.js
@@ -4,8 +4,10 @@ const host = '/amamy';
 // console.log(location);
 
 window.addEventListener('DOMContentLoaded', function () {
-    const homeLink = document.querySelector(
+    const homeLinks = document.querySelectorAll(
         'header .logo a, .main-content a.come-back.mobile'
     );
-    homeLink.href = env == 'dev' ? '/' : host;
+    homeLinks.forEach((link) => {
+        link.href = env == 'dev' ? '/' : host;
+    });
 });
