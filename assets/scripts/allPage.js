2f33865614f237bbf3853029a88d30acc1170b54
ninhnam <ninhnamyb2001@gmail.com>
2024-03-13 13:18:38 +0700

    abc

--- assets/scripts/allPage.js
+++ assets/scripts/allPage.js
@@ -3,5 +3,7 @@ const env = 'prod';
 const host = '/amamy';
 // console.log(location);
 window.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('header .logo a').href = env == 'dev' ? '/' : host;
+    document.querySelector(
+        'header .logo a, .main-content a.come-back.mobile'
+    ).href = env == 'dev' ? '/' : host;
 });
